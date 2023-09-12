5f2a9fd26d19b2791c70afaaec8507613be379be
Arold Mauger <aroldmauger2022@gmail.com>
2023-09-12 17:27:55 +0200

    Mise en place de fetch, récupération des données du fichier json, ajout des éléments des UserCard en JS

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,30 +1,34 @@
-    async function getPhotographers() {
+     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+
+        const response = await fetch('/data/photographers.json');
+        const photographers = await response.json();
+/*
         let photographers = [
             {
-                "name": "Ma data test",
+                "name": "Mimi Keel",
                 "id": 1,
                 "city": "Paris",
                 "country": "France",
-                "tagline": "Ceci est ma data test",
+                "tagline": "Photographe freelance",
                 "price": 400,
-                "portrait": "account.png"
+                "portrait": "MimiKeel.jpg"
             },
             {
-                "name": "Autre data test",
+                "name": "Marcel Nikolic",
                 "id": 2,
                 "city": "Londres",
                 "country": "UK",
-                "tagline": "Ceci est ma data test 2",
+                "tagline": "Photographe spécialiste",
                 "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+                "portrait": "MarcelNikolic.jpg"
+            }
+        ] 
+        // et bien retourner le tableau photographers seulement une fois récupéré */
+        return photographers
     }
+
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
+ function photographerTemplate(data) {
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,11 +7,34 @@ function photographerTemplate(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+
         const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+        h2.textContent = name;              
+
+        const location = document.createElement( 'p' );  // AJOUT DES ELEMENTS POUR COMPLETER LES CARDS SUR LA PAGE D'ACCUEIL
+        location.textContent = city + ", " + country;
+        location.classList.add("location");
+
+
+        const taglineText = document.createElement('p');
+        taglineText.textContent = tagline;
+        taglineText.classList.add("tagline");
+
+
+        const pricePerDay = document.createElement('span');
+        pricePerDay.textContent = price + "€/jour";
+
+
         article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(h2);  
+        article.appendChild(location);  
+        article.appendChild(taglineText);  
+        article.appendChild(pricePerDay);  
+
+
+
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
