93b3c3c6e34e69ed2b5caf266f123e8b97905854
Arold Mauger <aroldmauger2022@gmail.com>
2023-09-16 22:55:18 +0200

    La gestion des filtres a été mise à part dans un fichier filters.js + fonctions importées dans photographer.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,15 +1,13 @@
+import { clickOnFilterButton, sortByPopularity  } from "../utils/filters.js";
+
+
 const urlParams = new URLSearchParams(window.location.search); // On récupère l'id de l'URL
 const photographerId = urlParams.get('id');
-const infoPhotographer = document.querySelector(".info-photographer");
 const photographerHeader = document.querySelector(".photograph-header")
 const headerInfo = document.querySelector(".header-info")
 const mediasContainer = document.querySelector(".medias")
-
-const currentFilter = document.querySelector("#current_filter");
-const allFilters = Array.from(document.querySelectorAll(".dropdown_content li button"));
-const buttonSortByTitle = document.querySelector("#sort-by-title");
 const buttonSortByPopularity = document.querySelector("#sort-by-popularity");
-const buttonSortByDate = document.querySelector("#sort-by-date");
+const buttonFilters = document.querySelector(".btn_drop");
 
 // Fonction pour afficher les détails du photographe
 async function displayPhotographerInfo(id) {
@@ -18,8 +16,6 @@ async function displayPhotographerInfo(id) {
     const photographers = photographersData.photographers;
 
     const photographer = photographers.find(photographer => photographer.id === parseInt(id));
-
-        //infoPhotographer.innerHTML = '';  
 
         // On affiche les informations du photographe
         const namePhotographer = document.createElement("p");
@@ -110,103 +106,8 @@ async function displayPhotographerMedias(id) {
 // On appelle la fonction pour afficher les détails du photographe avec l'ID récupéré de l'URL
 displayPhotographerMedias(photographerId);
 
-
-
-//GESTION DE L'OUVERTURE ET FERMETURE DES FILTRES
-
-const dropDownFilters = document.querySelector(".dropdown_content");
-const buttonFilters = document.querySelector(".btn_drop");
-const chevronUp = document.querySelector(".fa-chevron-up")
-
+// Evenement au clic sur le bouton des filtres
 buttonFilters.addEventListener("click", clickOnFilterButton)
 
-
-function clickOnFilterButton () {
-    openFilters();
-    changingCurrentFilter();
-}
-
-function openFilters() {
-    if(dropDownFilters.style.display === "none") {
-        dropDownFilters.style.display = "block";
-        chevronUp.classList.add("rotate");
-
-    } else {
-        dropDownFilters.style.display = "none";
-        chevronUp.classList.remove("rotate");
-
-    }
-}
-
-function changingCurrentFilter () {
-    let selectedFilter = allFilters.find(filter => filter.textContent == currentFilter.textContent) // on cherche le filtre qui a le même nom que celui selectionné
-    
-        selectedFilter.style.display = 'none';  //On met le filtre sélectionné dans les 3 options du dropdown en display:none par défaut.
-
-
-        allFilters.forEach(filter => {
-            filter.addEventListener('click', function() {         // quand on cliquera sur chacun des filtres
-    
-                currentFilter.textContent = filter.textContent;   // le texte du currentFilter est mis à jour et correspond à celui du filtre cliqué
-                if (selectedFilter) selectedFilter.style.display = 'block'; // si c'est le filtre dont le contenu correspond au current, on l'affiche
-    
-                selectedFilter = filter;                    // selectedFilter contient le filtre cliqué
-                selectedFilter.style.display = 'none';      // le filtre actuellement selectionné est caché
-    
-            })
-        }); 
-
-    }
-
-// TRIER LES MEDIAS EN FONCTION DU NOMBRE DE LIKES
-
-buttonSortByPopularity.addEventListener("click", async function () {   
-    const response = await fetch('/data/photographers.json'); 
-    const photographersData = await response.json();
-    const medias = photographersData.media;
-    const mediasByPopularity = Array.from(medias);
-
-    mediasByPopularity.sort(function (a, b) {
-        return a.likes - b.likes;
-    });
-
-    mediasContainer.innerHTML = "";
-
-    for (let i = 0; i < mediasByPopularity.length; i++) {
-        const media = mediasByPopularity[i];
-
-        if (media.photographerId === parseInt(photographerId)) {
-
-
-        const cardContainer = document.createElement("div");
-        cardContainer.classList.add("card-container");
-
-        const likesContainer = document.createElement("div");
-        likesContainer.classList.add("likes-container");
-        const titleContainer = document.createElement("div");
-        titleContainer.classList.add("title-container");
-
-        const mediaFromPhotographer = document.createElement("img");
-        mediaFromPhotographer.setAttribute("src", `assets/Photographers_ID_Photos/${media.image}`);
-        mediaFromPhotographer.classList.add("photo-from-photographer");
-
-        const titleMedia = document.createElement("p");
-        titleMedia.textContent = media.title;
-
-        const heartIcone = document.createElement("i");
-        heartIcone.className = "fa-solid fa-heart";
-
-        const likesPhotographer = document.createElement("span");
-        likesPhotographer.textContent = media.likes;
-
-        likesContainer.appendChild(likesPhotographer);
-        titleContainer.appendChild(likesContainer);
-        titleContainer.appendChild(titleMedia);
-        likesContainer.appendChild(heartIcone);
-        cardContainer.appendChild(titleContainer);
-        cardContainer.appendChild(mediaFromPhotographer);
-        mediasContainer.appendChild(cardContainer);
-    }
-}
-});
-
+// Evenement au clic sur le bouton filtre "Popularité"
+buttonSortByPopularity.addEventListener("click", sortByPopularity)
---
+++ scripts/utils/filters.js
@@ -0,0 +1,103 @@
+//GESTION DES FILTRES
+const urlParams = new URLSearchParams(window.location.search); // On récupère l'id de l'URL
+const photographerId = urlParams.get('id');
+const currentFilter = document.querySelector("#current_filter");
+const allFilters = Array.from(document.querySelectorAll(".dropdown_content li button"));
+const dropDownFilters = document.querySelector(".dropdown_content");
+const chevronUp = document.querySelector(".fa-chevron-up")
+const mediasContainer = document.querySelector(".medias")
+
+
+//GESTION DE L'OUVERTURE ET CHANGEMENT DE NOM DES FILTRES
+
+
+
+ export function clickOnFilterButton () {
+    openFilters();
+    changingCurrentFilter();
+}
+
+export function openFilters() {
+    if(dropDownFilters.style.display === "none") {
+        dropDownFilters.style.display = "block";
+        chevronUp.classList.add("rotate");
+
+    } else {
+        dropDownFilters.style.display = "none";
+        chevronUp.classList.remove("rotate");
+
+    }
+}
+
+export function changingCurrentFilter () {
+    let selectedFilter = allFilters.find(filter => filter.textContent == currentFilter.textContent) // on cherche le filtre qui a le même nom que celui selectionné
+    
+        selectedFilter.style.display = 'none';  //On met le filtre sélectionné dans les 3 options du dropdown en display:none par défaut.
+
+
+        allFilters.forEach(filter => {
+            filter.addEventListener('click', function() {         // quand on cliquera sur chacun des filtres
+    
+                currentFilter.textContent = filter.textContent;   // le texte du currentFilter est mis à jour et correspond à celui du filtre cliqué
+                if (selectedFilter) selectedFilter.style.display = 'block'; // si c'est le filtre dont le contenu correspond au current, on l'affiche
+    
+                selectedFilter = filter;                    // selectedFilter contient le filtre cliqué
+                selectedFilter.style.display = 'none';      // le filtre actuellement selectionné est caché
+    
+            })
+        }); 
+
+    }
+
+// TRIER LES MEDIAS EN FONCTION DU NOMBRE DE LIKES
+
+ export async function sortByPopularity() {   
+    const response = await fetch('/data/photographers.json'); 
+    const photographersData = await response.json();
+    const medias = photographersData.media;
+    const mediasByPopularity = Array.from(medias);
+
+    mediasByPopularity.sort(function (a, b) {
+        return a.likes - b.likes;
+    });
+
+    mediasContainer.innerHTML = "";
+
+    for (let i = 0; i < mediasByPopularity.length; i++) {
+        const media = mediasByPopularity[i];
+
+        if (media.photographerId === parseInt(photographerId)) {
+
+
+        const cardContainer = document.createElement("div");
+        cardContainer.classList.add("card-container");
+
+        const likesContainer = document.createElement("div");
+        likesContainer.classList.add("likes-container");
+        const titleContainer = document.createElement("div");
+        titleContainer.classList.add("title-container");
+
+        const mediaFromPhotographer = document.createElement("img");
+        mediaFromPhotographer.setAttribute("src", `assets/Photographers_ID_Photos/${media.image}`);
+        mediaFromPhotographer.classList.add("photo-from-photographer");
+
+        const titleMedia = document.createElement("p");
+        titleMedia.textContent = media.title;
+
+        const heartIcone = document.createElement("i");
+        heartIcone.className = "fa-solid fa-heart";
+
+        const likesPhotographer = document.createElement("span");
+        likesPhotographer.textContent = media.likes;
+
+        likesContainer.appendChild(likesPhotographer);
+        titleContainer.appendChild(likesContainer);
+        titleContainer.appendChild(titleMedia);
+        likesContainer.appendChild(heartIcone);
+        cardContainer.appendChild(titleContainer);
+        cardContainer.appendChild(mediaFromPhotographer);
+        mediasContainer.appendChild(cardContainer);
+    }
+  }
+}
+
