2371eb8a5c60616331c691222c530bb67de3ffbb
Arold Mauger <aroldmauger2022@gmail.com>
2023-09-16 21:30:19 +0200

    Gestion du changement du nom de filtre au clic

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -5,6 +5,10 @@ const photographerHeader = document.querySelector(".photograph-header")
 const headerInfo = document.querySelector(".header-info")
 const mediasContainer = document.querySelector(".medias")
 
+const currentFilter = document.querySelector("#current_filter");
+const allFilters = Array.from(document.querySelectorAll(".dropdown_content li button"));
+
+
 // Fonction pour afficher les détails du photographe
 async function displayPhotographerInfo(id) {
     const response = await fetch('/data/photographers.json'); 
@@ -112,8 +116,13 @@ const dropDownFilters = document.querySelector(".dropdown_content");
 const buttonFilters = document.querySelector(".btn_drop");
 const chevronUp = document.querySelector(".fa-chevron-up")
 
-buttonFilters.addEventListener("click", openFilters)
+buttonFilters.addEventListener("click", clickOnFilterButton)
 
+
+function clickOnFilterButton () {
+    openFilters();
+    changingCurrentFilter();
+}
 
 function openFilters() {
     if(dropDownFilters.style.display === "none") {
@@ -126,3 +135,27 @@ function openFilters() {
 
     }
 }
+
+function changingCurrentFilter () {
+    let selectedFilter = allFilters.find(filter => filter.textContent == currentFilter.textContent) // on cherche le filtre qui a le même nom que celui selectionné
+    
+        selectedFilter.style.display = 'none';  //On met le filtre sélectionné dans les 3 options du dropdown en display:none par défaut.
+
+
+        allFilters.forEach(filter => {
+            filter.addEventListener('click', function() {         // quand on cliquera sur chacun des filtres
+    
+                currentFilter.textContent = filter.textContent;   // le texte du currentFilter est mis à jour et correspond à celui du filtre cliqué
+                if (selectedFilter) selectedFilter.style.display = 'block'; // si c'est le filtre dont le contenu correspond au current, on l'affiche
+    
+                selectedFilter = filter;                    // selectedFilter contient le filtre cliqué
+                selectedFilter.style.display = 'none';      // le filtre actuellement selectionné est caché
+    
+            })
+        }); 
+    }
+
+  
+
+
+
