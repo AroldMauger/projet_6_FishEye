06c4c5238a4c7325d7b716fc417d7889a71663d4
Arold Mauger <aroldmauger2022@gmail.com>
2023-09-18 14:11:36 +0200

    Affichage des réponses du formulaire de contact dans la console

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,3 +1,8 @@
+const firstName = document.querySelector("#first-name");
+const lastName = document.querySelector("#last-name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const form = document.querySelector("form")
 
 
 function displayModal() {
@@ -9,3 +14,31 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+
+form.addEventListener("submit", validate);
+
+function validate(event) {
+    event.preventDefault();
+    const firstNameValue = firstName.value;
+    const lastNameValue = lastName.value;
+    const emailValue = email.value;
+    const messageValue = message.value;
+
+    console.log("Prénom:", firstNameValue);
+    console.log("Nom de famille:", lastNameValue);
+    console.log("Email:", emailValue);
+    console.log("Message:", messageValue);
+
+    const firstNameIsValid = firstName.value.trim().length >= 2;
+    const lastNameIsValid = lastName.value.trim().length >= 2;
+    const emailIsValid = email.checkValidity();
+  
+    if (firstNameIsValid && lastNameIsValid && emailIsValid ) {
+  
+    closeModal()  
+    } else {
+      throw new Error("Formulaire non valide");
+    }
+  }
+  
