3c07ee4b2674096a64b2b87e816dbb0a0da07827
Arold Mauger <aroldmauger2022@gmail.com>
2023-09-25 11:54:04 +0200

    Séparation de la gestion des filtres dans un fichier .js séparé

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,21 +1,17 @@
-import {openLightboxPopularity, openLightboxName, openLightboxDate  } from "../utils/lightbox.js";
+import {openLightboxPopularity } from "../utils/lightbox.js";
 import {incrementLikes} from"../utils/likes.js";
+
 
 const urlParams = new URLSearchParams(window.location.search); // On récupère l'id de l'URL
 const photographerId = urlParams.get("id");
 const photographerHeader = document.querySelector(".photograph-header");
 const headerInfo = document.querySelector(".header-info");
 const mediasContainer = document.querySelector(".medias");
-const buttonSortByPopularity = document.querySelector("#sort-by-popularity");
-const buttonSortByTitle = document.querySelector("#sort-by-title");
-const buttonSortByDate = document.querySelector("#sort-by-date");
-const buttonFilters = document.querySelector(".btn_drop");
+
 const nameInModalContainer = document.querySelector(".name-in-contactModal");
 const dropDownFilters = document.querySelector(".dropdown_content");
 const chevronUp = document.querySelector(".fa-chevron-down");
-const allFilters = Array.from(document.querySelectorAll(".dropdown_content li button"));
-const currentFilter = document.querySelector("#current_filter");
-const buttonHeader = document.querySelector(".button-header");
+
 
 export async function fetchData() {
 	const response = await fetch("/data/photographers.json"); 
@@ -88,23 +84,10 @@ fetchData().then(photographersData => {
 
 
 // Evenement au clic sur le bouton des filtres
-buttonFilters.addEventListener("click", clickOnFilterButton);
-
-buttonSortByPopularity.addEventListener("click", sortByPopularity);
-buttonSortByTitle.addEventListener("click", sortByTitle);
-buttonSortByDate.addEventListener("click", sortByDate);
 
 
 
-
-function clickOnFilterButton () {
-	openFilters();
-	changingCurrentFilter();
-}
-
-
-
-function createCardElement(media) {
+export function createCardElement(media) {
 	
 
 	const divCardContainer = document.createElement("div");
@@ -171,114 +154,6 @@ function createCardElement(media) {
 	
 
 }
-
-// GESTION DES FILTRES
-function openFilters() {
-	if(dropDownFilters.style.display === "none") {
-		dropDownFilters.style.display = "block";
-		chevronUp.classList.add("rotate");
-		dropDownFilters.setAttribute("aria-hidden", "false");
-		buttonFilters.setAttribute("aria-expanded", "true");
-		dropDownFilters.setAttribute("aria-activedescendant", "true");
-	} else {
-		dropDownFilters.style.display = "none";
-		chevronUp.classList.remove("rotate");
-		dropDownFilters.setAttribute("aria-hidden", "true");
-		buttonFilters.setAttribute("aria-expanded", "false");
-		dropDownFilters.removeAttribute("aria-activedescendant");
-	}
-}
-
-
-
-/* On ferme les filtres si on clique en dehors des filtres */
-window.addEventListener("click", function(e) {
-	if (e.target !== buttonFilters && e.target !== dropDownFilters && e.target !== currentFilter && e.target !== chevronUp) {
-		dropDownFilters.style.display = "none";
-		chevronUp.classList.remove("rotate");
-		dropDownFilters.setAttribute("aria-hidden", "true");
-	}
-});
-
-/* On ferme les filtres quand le bouton "Contactez-moi" est focuse */
-
-buttonHeader.addEventListener("focus", function() {   
-	dropDownFilters.style.display = "none";
-	chevronUp.classList.remove("rotate");
-	dropDownFilters.setAttribute("aria-hidden", "true");
-});
-
-
-function changingCurrentFilter() {
-	let selectedFilter = allFilters.find(filter => filter.textContent === currentFilter.textContent);
-	selectedFilter.style.display = "none";
-	allFilters.forEach(filter => {
-		filter.addEventListener("click", function () {
-			currentFilter.textContent = filter.textContent;
-
-			if (selectedFilter) selectedFilter.style.display = "block";
-			selectedFilter = filter;
-			selectedFilter.style.display = "none";
-		});
-		filter.addEventListener("focus", function(){
-			filter.setAttribute("aria-activedescendant", "true");
-		});
-		selectedFilter.addEventListener("focus", function(){
-			filter.setAttribute("aria-selected", "true");
-		});
-	});
-	
-}
-
-
-async function sortByPopularity() {
-	const photographersData = await fetchData();
-	const medias = photographersData.media;
-	const mediasByPopularity = [...medias].sort((a, b) => a.likes - b.likes);
-  
-	mediasContainer.innerHTML = "";
-	mediasByPopularity.forEach(media => {
-		if (media.photographerId === parseInt(photographerId)) {
-			createCardElement(media);
-		}
-	});
-	openLightboxPopularity();
-	incrementLikes(photographerId, photographersData);
-
-}
-  
-async function sortByTitle() {
-	const photographersData = await fetchData();
-	const medias = photographersData.media;
-	const mediasByTitle = [...medias].sort((a, b) => a.title.localeCompare(b.title));
-  
-	mediasContainer.innerHTML = "";
-	mediasByTitle.forEach(media => {
-		if (media.photographerId === parseInt(photographerId)) {
-			createCardElement(media);
-		}
-	});
-	openLightboxName();
-	incrementLikes(photographerId, photographersData);
-
-}
-  
-async function sortByDate() {
-	const photographersData = await fetchData();
-	const medias = photographersData.media;
-	const mediasByDate = [...medias].sort((a, b) => new Date(b.date) - new Date(a.date));
-  
-	mediasContainer.innerHTML = "";
-	mediasByDate.forEach(media => {
-		if (media.photographerId === parseInt(photographerId)) {
-			createCardElement(media);
-        
-		}
-	});
-	openLightboxDate();
-	incrementLikes(photographerId, photographersData);
-}
-
 
 
   
--- scripts/utils/filters.js
+++ scripts/utils/filters.js
@@ -1,106 +1,134 @@
-/*
-import { fetchData } from "../pages/photographer.js";
+import {fetchData, createCardElement } from "../pages/photographer.js";
+import {openLightboxPopularity, openLightboxName, openLightboxDate } from "../utils/lightbox.js";
+import {incrementLikes} from "../utils/likes.js";
+const buttonSortByPopularity = document.querySelector("#sort-by-popularity");
+const buttonSortByTitle = document.querySelector("#sort-by-title");
+const buttonSortByDate = document.querySelector("#sort-by-date");
 
 //GESTION DES FILTRES
-
-
 const urlParams = new URLSearchParams(window.location.search); // On récupère l'id de l'URL
 const photographerId = urlParams.get('id');
 const currentFilter = document.querySelector("#current_filter");
 const allFilters = Array.from(document.querySelectorAll(".dropdown_content li button"));
 const dropDownFilters = document.querySelector(".dropdown_content");
-const chevronUp = document.querySelector(".fa-chevron-up")
-const mediasContainer = document.querySelector(".medias")
+const chevronUp = document.querySelector(".fa-chevron-down");
+const mediasContainer = document.querySelector(".medias");
+const buttonHeader = document.querySelector(".button-header");
+const buttonFilters = document.querySelector(".btn_drop");
 
-
-//GESTION DE L'OUVERTURE ET CHANGEMENT DE NOM DES FILTRES
-
- export function clickOnFilterButton () {
-    openFilters();
-    changingCurrentFilter();
-}
-
+// GESTION DES FILTRES
 export function openFilters() {
-    if(dropDownFilters.style.display === "none") {
-        dropDownFilters.style.display = "block";
-        chevronUp.classList.add("rotate");
-
-    } else {
-        dropDownFilters.style.display = "none";
-        chevronUp.classList.remove("rotate");
-
-    }
+	if(dropDownFilters.style.display === "none") {
+		dropDownFilters.style.display = "block";
+		chevronUp.classList.add("rotate");
+		dropDownFilters.setAttribute("aria-hidden", "false");
+		buttonFilters.setAttribute("aria-expanded", "true");
+		dropDownFilters.setAttribute("aria-activedescendant", "true");
+	} else {
+		dropDownFilters.style.display = "none";
+		chevronUp.classList.remove("rotate");
+		dropDownFilters.setAttribute("aria-hidden", "true");
+		buttonFilters.setAttribute("aria-expanded", "false");
+		dropDownFilters.removeAttribute("aria-activedescendant");
+	}
 }
 
-export function changingCurrentFilter () {
-    let selectedFilter = allFilters.find(filter => filter.textContent == currentFilter.textContent) // on cherche le filtre qui a le même nom que celui selectionné
-    
-        selectedFilter.style.display = 'none';  //On met le filtre sélectionné dans les 3 options du dropdown en display:none par défaut.
+
+/* On ferme les filtres si on clique en dehors des filtres */
+window.addEventListener("click", function(e) {
+	if (e.target !== buttonFilters && e.target !== dropDownFilters && e.target !== currentFilter && e.target !== chevronUp) {
+		dropDownFilters.style.display = "none";
+		chevronUp.classList.remove("rotate");
+		dropDownFilters.setAttribute("aria-hidden", "true");
+	}
+});
+
+/* On ferme les filtres quand le bouton "Contactez-moi" est focuse */
+
+buttonHeader.addEventListener("focus", function() {   
+	dropDownFilters.style.display = "none";
+	chevronUp.classList.remove("rotate");
+	dropDownFilters.setAttribute("aria-hidden", "true");
+});
 
 
-        allFilters.forEach(filter => {
-            filter.addEventListener('click', function() {         // quand on cliquera sur chacun des filtres
-    
-                currentFilter.textContent = filter.textContent;   // le texte du currentFilter est mis à jour et correspond à celui du filtre cliqué
-                if (selectedFilter) selectedFilter.style.display = 'block'; // si c'est le filtre dont le contenu correspond au current, on l'affiche
-    
-                selectedFilter = filter;                    // selectedFilter contient le filtre cliqué
-                selectedFilter.style.display = 'none';      // le filtre actuellement selectionné est caché
-    
-            })
-        }); 
+export function changingCurrentFilter() {
+	let selectedFilter = allFilters.find(filter => filter.textContent === currentFilter.textContent);
+	selectedFilter.style.display = "none";
+	allFilters.forEach(filter => {
+		filter.addEventListener("click", function () {
+			currentFilter.textContent = filter.textContent;
 
-    }
+			if (selectedFilter) selectedFilter.style.display = "block";
+			selectedFilter = filter;
+			selectedFilter.style.display = "none";
+		});
+		filter.addEventListener("focus", function(){
+			filter.setAttribute("aria-activedescendant", "true");
+		});
+		selectedFilter.addEventListener("focus", function(){
+			filter.setAttribute("aria-selected", "true");
+		});
+	});
+	
+}
 
-// TRIER LES MEDIAS EN FONCTION DU NOMBRE DE LIKES
-async function sortByPopularity() {
-    const photographersData = await fetchData();
-    const medias = photographersData.media;
-    const mediasByPopularity = [...medias].sort((a, b) => a.likes - b.likes);
+buttonFilters.addEventListener("click", clickOnFilterButton);
+
+function clickOnFilterButton () {
+	openFilters();
+	changingCurrentFilter();
+}
+
+export async function sortByPopularity() {
+	const photographersData = await fetchData();
+	const medias = photographersData.media;
+	const mediasByPopularity = [...medias].sort((a, b) => a.likes - b.likes);
   
-    mediasContainer.innerHTML = "";
-    mediasByPopularity.forEach(media => {
-      if (media.photographerId === parseInt(photographerId)) {
-        createCardElement(media);
-      }
-    });
-    openLightboxPopularity();
-  }
+	mediasContainer.innerHTML = "";
+	mediasByPopularity.forEach(media => {
+		if (media.photographerId === parseInt(photographerId)) {
+			createCardElement(media);
+		}
+	});
+	openLightboxPopularity();
+	incrementLikes(photographerId, photographersData);
 
-
-// TRIER LES MEDIAS EN FONCTION DE LEUR NOM
-
-export async function sortByTitle() {   
-    const photographersData = await fetchData();
-    const medias = photographersData.media;
-    const mediasByTitle = [...medias].sort((a, b) => a.title.localeCompare(b.title));
-
-    mediasContainer.innerHTML = "";
-
-    mediasByTitle.forEach(media => {
-    if (media.photographerId === parseInt(photographerId)) {
-        createCardElement(media);
-    }
-  });
-  openLightbox();
 }
-
-// TRIER LES MEDIAS EN FONCTION DE LEUR DATE
-
-export async function sortByDate() {   
-    const photographersData = await fetchData();
-    const medias = photographersData.media;
-    const mediasByDate = [...medias].sort((a, b) => new Date(b.date) - new Date(a.date));
-
-
-    mediasContainer.innerHTML = "";
-    mediasByDate.forEach(media => {
-        if (media.photographerId === parseInt(photographerId)) {
-          createCardElement(media);
-        }
-      });
   
-  openLightboxDate()
+export async function sortByTitle() {
+	const photographersData = await fetchData();
+	const medias = photographersData.media;
+	const mediasByTitle = [...medias].sort((a, b) => a.title.localeCompare(b.title));
+  
+	mediasContainer.innerHTML = "";
+	mediasByTitle.forEach(media => {
+		if (media.photographerId === parseInt(photographerId)) {
+			createCardElement(media);
+		}
+	});
+	openLightboxName();
+	incrementLikes(photographerId, photographersData);
+
+}
+  
+export async function sortByDate() {
+	const photographersData = await fetchData();
+	const medias = photographersData.media;
+	const mediasByDate = [...medias].sort((a, b) => new Date(b.date) - new Date(a.date));
+  
+	mediasContainer.innerHTML = "";
+	mediasByDate.forEach(media => {
+		if (media.photographerId === parseInt(photographerId)) {
+			createCardElement(media);
+        
+		}
+	});
+	openLightboxDate();
+	incrementLikes(photographerId, photographersData);
 }
 
-*/
+
+buttonSortByPopularity.addEventListener("click", sortByPopularity);
+buttonSortByTitle.addEventListener("click", sortByTitle);
+buttonSortByDate.addEventListener("click", sortByDate);
