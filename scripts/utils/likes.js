b29f99a59e520aff03ee98db48f2743366304d68
Arold Mauger <aroldmauger2022@gmail.com>
2023-09-25 11:41:25 +0200

    Séparation de la fonction incrementLikes() dans un autre fichier .js, ajout effet opacity sur les cards

--- scripts/utils/likes.js
+++ scripts/utils/likes.js
@@ -1,48 +1,65 @@
-/*
-export async function incrementLikes (totalLikes) {
+const totalLikesAndPrice = document.querySelector("aside");
+let totalLikes = 0;
 
-    const likesButtons = document.querySelectorAll(".likes-button");
+export function incrementLikes(id, photographersData) {
 
-    likesButtons.forEach(likesButton => {
-        likesButton.addEventListener("click", async function() {
-            const response = await fetch('/data/photographers.json'); 
-            const photographersData = await response.json();
-            const medias = photographersData.media;
-    
-            const mediaId = likesButton.id;                          // On veut que l'id du media corresponde à celui de son bouton    
-            const media = medias.find(media => media.id == mediaId);
-    
-    
-            if (likesButton.getAttribute("liked") === "false") {    // Si l'attribut du bouton est toujours sur false
-                media.likes++;                                      // On incrémente
-                likesButton.setAttribute("liked", "true");          // On actualise l'attribut du bouton sur true
-                totalLikes++;
-              
-           
-            const numberOfTotalLikes = document.querySelector(".number-of-total-likes");
-            numberOfTotalLikes.textContent = totalLikes.toString();
+	// FAIRE APPARAITRE LE NOMBRE TOTAL DE LIKES ET LE PRIX PAR JOUR
+	const medias = photographersData.media;
+	const photographer = photographersData.photographers.find(photographer => photographer.id === parseInt(id));
+	const filteredMedias = medias.filter(media => media.photographerId === parseInt(id)); // Utilisez 'id' au lieu de 'photographerId'
 
-            console.log(numberOfTotalLikes)
+	totalLikes = filteredMedias.reduce((total, media) => total + media.likes, 0);
 
-            const likesPhotographer = document.querySelectorAll(".number-of-likes");
-          
-            likesPhotographer.forEach(likePhotographer => {
-    
-            if(likePhotographer.id === mediaId) {
-                const likesCount = parseInt(likePhotographer.textContent, 10); // On convertit la chaîne de caractères en nombre avec parseInt
-                likePhotographer.textContent = (likesCount + 1).toString();  // On ajoute 1 et on reconvertit en chaîne de caractères
-            }
+	const divInAside = document.createElement("div");
+	divInAside.classList.add("div-in-aside");
 
-            const heartIcone = likesButton.querySelector(".fa-heart");
-                heartIcone.style.color = "#901C1C";
-        });
-    }});
-    
-    })
+	const numberOfTotalLikes = document.createElement("span");
+	numberOfTotalLikes.textContent = totalLikes.toString();
+	numberOfTotalLikes.classList.add("number-of-total-likes");
 
+	const heartInAside = document.createElement("i");
+	heartInAside.className = "fa-solid fa-heart";
+
+	const price = document.createElement("span");
+	price.textContent = `${photographer.price} / jour`;
+
+	totalLikesAndPrice.innerHTML = "";
+	divInAside.appendChild(numberOfTotalLikes);
+	divInAside.appendChild(heartInAside);
+	totalLikesAndPrice.appendChild(divInAside);
+
+	totalLikesAndPrice.appendChild(price);
+
+	const likesButtons = document.querySelectorAll(".likes-button");
+
+	
+  
+	likesButtons.forEach(likesButton => {
+		likesButton.addEventListener("click",  function () {
+      
+
+			const mediaId = likesButton.id;
+			const media = medias.find(media => media.id == mediaId);
+
+			if (likesButton.getAttribute("liked") === "false") {
+				media.likes++;
+				likesButton.setAttribute("liked", "true");
+				totalLikes++;
+				numberOfTotalLikes.textContent = totalLikes.toString();
+
+
+				const likesPhotographer = document.querySelectorAll(".number-of-likes");
+
+				likesPhotographer.forEach(likePhotographer => {
+					if (likePhotographer.id === mediaId) {
+						const likesCount = parseInt(likePhotographer.textContent, 10);
+						likePhotographer.textContent = (likesCount + 1).toString();
+					}
+
+					const heartIcone = likesButton.querySelector(".fa-heart");
+					heartIcone.style.color = "#901C1C";
+				});
+			}
+		});
+	});
 }
-
-
-    */    
-
-
